be63a959f1a2c5b083c7ea32e89bbdab9e5502d0
Danyal <muhammaddanyal429@gmail.com>
2025-06-25 18:26:11 +0500

    Add initial implementation of pycaret_model_trainer component

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -32,7 +32,7 @@ class ModelTrainer:
         try:
             logging.info("Splitting training and testing data")
             X_train, y_train, X_test, y_test = (
-                train_array[:, :-1],
+                train_array[:, :, -1],
                 train_array[:, -1],
                 test_array[:, :-1],
                 test_array[:, -1],
